743f06252105a2c580a3367536d3a2f392788f36
sagesarchives <nylalanise05@gmail.com>
2026-09-01 11:31:27 -0400

    Initial Commit

---
+++ GitHubTutorial/GitHubTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTutorialApp.swift
+//  GitHubTutorial
+//
+//  Created by Nyla Wilson on 9/1/26.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GitHubTutorialTests/GitHubTutorialTests.swift
@@ -0,0 +1,19 @@
+//
+//  GitHubTutorialTests.swift
+//  GitHubTutorialTests
+//
+//  Created by Nyla Wilson on 9/1/26.
+//
+
+import Testing
+@testable import GitHubTutorial
+
+struct GitHubTutorialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
